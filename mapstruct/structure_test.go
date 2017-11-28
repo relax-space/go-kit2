93c16f85b5ad369a3d28fca963c925552178fd95
xiao.xinmiao <xiaoxm_001@163.com>
2017-11-29 07:26:22 +0800

    add method NewUuid

--- mapstruct/structure_test.go
+++ mapstruct/structure_test.go
@@ -1,6 +1,7 @@
 package mapstruct
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
@@ -17,4 +18,79 @@ func Test_StructToMap(t *testing.T) {
 	param := s.Map()
 	fmt.Printf("\n%+v", param)
 
+}
+
+func Test_StructToMap_2(t *testing.T) {
+
+	type Dto2 struct {
+		Name string `json:"name"`
+		Age  int64  `json:"age"`
+	}
+
+	type Dto1 struct {
+		Name  string `json:"name"`
+		Param Dto2   `json:"dto2"`
+	}
+
+	var reqDto = Dto1{
+		Name: "123",
+		Param: Dto2{
+			Name: "sub_123",
+			Age:  12,
+		},
+	}
+	s := structs.New(reqDto)
+	s.TagName = "json"
+	param := s.Map()
+	fmt.Printf("\n%+v", param)
+
+}
+
+func Test_StructToMap_3(t *testing.T) {
+
+	type BaseDto struct {
+		Name string `json:"name"`
+		Age  int64  `json:"age"`
+	}
+
+	type Dto1 struct {
+		*BaseDto `json:"-"`
+		Name     string `json:"name"`
+	}
+
+	var reqDto = Dto1{
+		BaseDto: &BaseDto{
+			Name: "sub_123",
+			Age:  12,
+		},
+		Name: "123",
+	}
+
+	s := structs.New(reqDto)
+	s.TagName = "json"
+	param := s.Map()
+	fmt.Printf("\n%+v", param)
+
+}
+
+func Test_Json(t *testing.T) {
+
+	type BaseDto struct {
+		Name string `json:"name"`
+		Age  int64  `json:"age"`
+	}
+
+	type Dto1 struct {
+		*BaseDto
+		Name1 string `json:"name1"`
+	}
+
+	jsonStr := `{
+		"name1":"123",
+		"name":"234"
+	}`
+	var dto1 Dto1
+	json.Unmarshal([]byte(jsonStr), &dto1)
+	fmt.Println(dto1)
+	fmt.Println(dto1.BaseDto)
 }
--- random/random.go
+++ random/random.go
@@ -3,7 +3,9 @@ package random
 import (
 	"bytes"
 	"encoding/binary"
+	"go-kit/base"
 	"strconv"
+	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -17,4 +19,8 @@ func UuIdInt64() (uuId uint64) {
 
 func Uuid(randomType string) string {
 	return randomType + strconv.FormatUint(UuIdInt64(), 10)
+}
+func NewUuid(randomType string) string {
+	t := time.Now()
+	return randomType + base.NewDateFormat(t, "yyMMdd") + strconv.FormatUint(UuIdInt64(), 10)
 }
